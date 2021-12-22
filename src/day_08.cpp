742bc2efed18bf5801805d0908e5810dafaf135d
thetomato2 <derekj.nord@gmail.com>
2021-12-21 19:24:41 -0600

    day 8 kinda works

--- src/day_08.cpp
+++ src/day_08.cpp
@@ -3,55 +3,150 @@
 namespace day08
 {
 
-std::vector<Signal>
-read_input_file(std::string input_path)
+Puzzle
+parse_line(const std::string& in)
 {
-	std::vector<Signal> signals;
-	std::string arg;
+	Puzzle result;
+	bool past_delim = false;
+	auto it			= in.begin();
+	while (it != in.end()) {
+		// read one number
+		Display d = 0;
+		while (it != in.end() && std::islower(*it)) {
+			if (*it - 'a' > 7)
+				throw std::out_of_range("Unexpected value in input, only accepts values 'a-g'.");
+			d |= 1 << (*it - 'a');
+			it++;
+		}
+		if (past_delim) {
+			result.numbers.push_back(d);
+		} else {
+			result.patterns.push_back(d);
+		}
+		// skip over spaces and delimiter '|'
+		while (it != in.end() && (std::isspace(*it) || *it == '|')) {
+			if (*it == '|') past_delim = true;
+			it++;
+		}
+	}
+	return result;
+}
 
-	std::ifstream input_file(input_path);
+std::vector<Puzzle>
+read_input_file(std::string in_path)
+{
+	std::vector<Puzzle> result;
+	std::ifstream input_file(in_path);
 	if (input_file.is_open()) {
 		std::string line;
 		while (std::getline(input_file, line)) {
-			Signal signal;
-			std::istringstream iss(line);
-			// std::cout << iss.str() << '\n';
-
-			std::string cur_str;
-			while (iss >> cur_str) {
-				if (cur_str == "|") break;
-				signal.signal_patterns.push_back(cur_str);
-			}
-			while (iss >> cur_str) {
-				signal.output.push_back(cur_str);
-			}
-			signals.push_back(signal);
+			auto puzzle = parse_line(line);
+			result.push_back(puzzle);
 		}
 	}
-	return signals;
+	return result;
 }
 
 i32
-do_part_01(std::vector<Signal>& input)
+do_part_01(std::vector<Puzzle>& in)
 {
-	i32 res {};
-	for (auto signal : input) {
-		for (auto output : signal.output) {
-			if (output.length() == 2 || output.length() == 3 || output.length() == 4 ||
-				output.length() == 7) {
-				++res;
-				std::cout << "res: " << res << '\n';
-			}
+	i32 result {};
+	for (auto p : in) {
+		result += std::ranges::count_if(p.numbers, [](auto n) {
+			u8 cnt = std::popcount(n);
+			return cnt == 2 || cnt == 3 || cnt == 4 || cnt == 7;
+		});
+	}
+
+	return result;
+}
+
+Wiring
+solve_wiring(const Puzzle& input)
+{
+	Wiring result = { NUMBERS[8], NUMBERS[8], NUMBERS[8], NUMBERS[8],
+					  NUMBERS[8], NUMBERS[8], NUMBERS[8] };
+	for (auto pattern : input.patterns) {
+		switch (cnt_segments(pattern)) {
+			case 2:
+				for (u8 wire_in = 0; wire_in < 7; wire_in++) {
+					if (segment_lit(pattern, wire_in)) {
+						result[wire_in] &= (SEGMENT_C | SEGMENT_F);
+					} else {
+						result[wire_in] &= ~(SEGMENT_C | SEGMENT_F);
+					}
+				}
+				break;
+			case 3:
+				for (u8 wire_in = 0; wire_in < 7; wire_in++) {
+					if (segment_lit(pattern, wire_in)) {
+						result[wire_in] &= (SEGMENT_A | SEGMENT_C | SEGMENT_F);
+					} else {
+						result[wire_in] &= ~(SEGMENT_A | SEGMENT_C | SEGMENT_F);
+					}
+				}
+				break;
+			case 4:
+				for (u8 wire_in = 0; wire_in < 7; wire_in++) {
+					if (segment_lit(pattern, wire_in)) {
+						result[wire_in] &= (SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_F);
+					} else {
+						result[wire_in] &= ~(SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_F);
+					}
+				}
+				break;
+			case 5:
+				for (u8 wire_in = 0; wire_in < 7; wire_in++) {
+					if (!segment_lit(pattern, wire_in)) {
+						result[wire_in] &= (SEGMENT_B | SEGMENT_C | SEGMENT_E | SEGMENT_F);
+					}
+				}
+				break;
+			case 6:
+				for (u8 wire_in = 0; wire_in < 7; wire_in++) {
+					if (!segment_lit(pattern, wire_in)) {
+						result[wire_in] &= (SEGMENT_C | SEGMENT_D | SEGMENT_E);
+					}
+				}
+				break;
 		}
 	}
-	return res;
+	for (auto singular_wire : result) {
+		if (std::popcount(singular_wire) != 1) continue;
+
+		for (auto& undecided_wire : result) {
+			if (std::popcount(undecided_wire) == 1) continue;
+			undecided_wire &= ~singular_wire;
+		}
+	}
+	return result;
+}
+
+u32
+decode_number(const Puzzle& puzzle, const Wiring& solved_wiring)
+{
+	u32 result = 0;
+	for (auto digit : puzzle.numbers) {
+		u8 display = 0;
+		for (u8 bit = 0; bit < 7; ++bit) {
+			if (segment_lit(digit, bit)) display |= solved_wiring[bit];
+		}
+
+		result *= 10;
+		result += DECIMALS.at(display);
+	}
+	return result;
 }
 
 i32
-do_part_02(std::vector<Signal>& input)
+do_part_02(std::vector<Puzzle>& in)
 {
-	std::cout << "Part 2 not implmented yet\n";
-	return 0;
+	i32 result;
+	for (auto p : in) {
+		Wiring w = solve_wiring(p);
+		result += decode_number(p, w);
+	}
+	return result;
 }
 
 }  // namespace day08
--- src/day_08.hpp
+++ src/day_08.hpp
@@ -5,37 +5,77 @@
 
 namespace day08
 {
-struct Signal
-{
-	std::vector<std::string> signal_patterns;
-	std::vector<std::string> output;
+
+using Display = u8;
+
+constexpr const Display SEGMENT_A = 1;
+constexpr const Display SEGMENT_B = 1 << 1;
+constexpr const Display SEGMENT_C = 1 << 2;
+constexpr const Display SEGMENT_D = 1 << 3;
+constexpr const Display SEGMENT_E = 1 << 4;
+constexpr const Display SEGMENT_F = 1 << 5;
+constexpr const Display SEGMENT_G = 1 << 6;
+
+constexpr const std::array<Display, 10> NUMBERS = {
+	SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_E | SEGMENT_F | SEGMENT_G,				// 0
+	SEGMENT_C | SEGMENT_F,																// 1
+	SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_G,							// 2
+	SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_F | SEGMENT_G,							// 3
+	SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_F,										// 4
+	SEGMENT_A | SEGMENT_B | SEGMENT_D | SEGMENT_F | SEGMENT_G,							// 5
+	SEGMENT_A | SEGMENT_B | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G,				// 6
+	SEGMENT_A | SEGMENT_C | SEGMENT_F,													// 7
+	SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G,	// 8
+	SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_F | SEGMENT_G,				// 9
 };
 
-struct Digit
+struct Puzzle
 {
-	Digit() { segments.resize(7); }
-
-	/*
-	 *  0000
-	 * 1    2
-	 * 1    2
-	 *  3333
-	 * 4    5
-	 * 4    5
-	 *  6666
-	 */
-
-	std::vector<std::string> segments;
+	std::vector<Display> patterns;
+	std::vector<Display> numbers;
 };
 
-std::vector<Signal>
+using Wiring = std::array<u8, 7>;
+
+inline const std::unordered_map<Display, u8> DECIMALS = {
+	{ NUMBERS[0], 0 }, { NUMBERS[1], 1 }, { NUMBERS[2], 2 }, { NUMBERS[3], 3 }, { NUMBERS[4], 4 },
+	{ NUMBERS[5], 5 }, { NUMBERS[6], 6 }, { NUMBERS[7], 7 }, { NUMBERS[8], 8 }, { NUMBERS[9], 9 }
+};
+
+inline bool
+segment_lit(Display disp, u8 segment)
+{
+	return disp & (1 << segment);
+}
+
+inline u8
+cnt_segments(Display display)
+{
+	return std::popcount(display);
+}
+
+uint32_t
+decode_number(const Puzzle& puzzle, const Wiring& solved_wiring);
+
+Wiring
+solve_wiring(const Puzzle& input);
+
+Wiring
+solve_wiring(const Puzzle&);
+uint32_t
+decode_number(const Puzzle&, const Wiring&);
+
+Puzzle
+parse_line(const std::string& in);
+
+std::vector<Puzzle>
 read_input_file(std::string input_path);
 
 i32
-do_part_01(std::vector<Signal>& input);
+do_part_01(std::vector<Puzzle>& input);
 
 i32
-do_part_02(std::vector<Signal>& input);
+do_part_02(std::vector<Puzzle>& input);
 
 }  // namespace day08
 
